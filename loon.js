e978947a5a50d5ce498b12aa450862a4c86c7161
canxueil <73378012+canxueil@users.noreply.github.com>
2021-05-18 16:33:03 +0800

    Update loon.js

--- loon.js
+++ loon.js
@@ -1,3 +1,2 @@
-hostname = api.m.jd.com
 
 http-response ^https?://api\.m\.jd\.com/client\.action\?functionId=(wareBusiness|serverConfig|basicConfig) script-path=https://raw.githubusercontent.com/JDHelloWorld/jd_price/main/jd_price.js, requires-body=true, timeout=10, tag=京东比价
